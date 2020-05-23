b70956a851294754a2934644f87126eb8f6d912a
Matheus <matheus@Deneb.ad.deneb.xyz>
2020-05-23 20:11:37 -0300

    fix permissions on home folder

--- docker/start_condor_and_run.sh
+++ docker/start_condor_and_run.sh
@@ -1,8 +1,11 @@
 #$(condor_config_val MASTER) -f -t >> /var/log/condor/MasterLog 2>&1
 echo "123456" | sudo -S condor_master >> /dev/null 2>&1
+sudo chown submitter -R /home/submitter
 sleep 5
 
 pegasus-service -H 0.0.0.0 -p 5000 &> /dev/null &
 
 cd pegasus
 python run_sequentially.py $1
+
+bash
