6c0ea5948e2fe6f9d6449284b3ce6b4adcab1ae2
Matheus <matheus@Deneb.ad.deneb.xyz>
2020-05-31 00:39:36 -0300

    fix pegasus start

--- docker/start_condor_and_run.sh
+++ docker/start_condor_and_run.sh
@@ -3,7 +3,7 @@ echo "123456" | sudo -S condor_master >> /dev/null 2>&1
 sudo chown submitter -R /home/submitter
 sleep 5
 
-pegasus-service -H 0.0.0.0 -p 5000 >> /dev/null 2>&1
+pegasus-service -H 0.0.0.0 -p 5000 >> /dev/null 2>&1 &
 
 cd pegasus
 python run_sequentially.py $1
