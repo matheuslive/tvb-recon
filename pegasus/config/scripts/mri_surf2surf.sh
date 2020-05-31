f7573ca03d8b8fa221d4d9d512c4495cd4da266a
Matheus <matheus@Deneb.ad.deneb.xyz>
2020-05-30 21:36:57 -0300

    Fix mri_surf2surf script to accept default atlas

--- pegasus/config/scripts/mri_surf2surf.sh
+++ pegasus/config/scripts/mri_surf2surf.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
 
+if [ $1 != "--srcsubject" ]
+then
+    atlas=$1
+    shift
+fi
+
 export FREESURFER_HOME
 export SUBJECTS_DIR
 source ${FREESURFER_HOME}/FreeSurferEnv.sh
-export TRGSUBJECT=$5
-export SUBJECT=$3
+export TRGSUBJECT=$4
+export SUBJECT=$2
 
 f=$PWD
 
-mri_surf2surf ${@:2}
+mri_surf2surf $@
 
-if [ $9 == "pial" ] || [ $9 == "white" ]
+if [ $8 == "pial" ] || [ $8 == "white" ]
 then
     cd ${SUBJECTS_DIR}/${TRGSUBJECT}/surf
-    if [ $7 == "lh" ]
+    if [ $6 == "lh" ]
     then
-        mv lh.$9-${TRGSUBJECT} $f
+        mv lh.$8-${TRGSUBJECT} $f
     else
-        mv rh.$9-${TRGSUBJECT} $f
+        mv rh.$8-${TRGSUBJECT} $f
     fi
 else
     cd ${SUBJECTS_DIR}/${TRGSUBJECT}/label
-    if [ $7 == "lh" ]
+    if [ $6 == "lh" ]
     then
-        cp lh.aparc-${TRGSUBJECT}$1.annot $f
+        cp lh.aparc-${TRGSUBJECT}$atlas.annot $f
     else
-        cp rh.aparc-${TRGSUBJECT}$1.annot $f
+        cp rh.aparc-${TRGSUBJECT}$atlas.annot $f
     fi
 fi
